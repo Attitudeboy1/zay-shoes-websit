bf1e3743b94d5c03af7c3693a50e41f45ec7bd19
Attitudeboy1 <tumharaemail@example.com>
2026-01-02 18:24:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var fname = prompt("firstName");
+var lname = prompt("lastName");
+ cansole.log("welcome" +fname+lname);
+ var num1 = number(prompt('Enter your number +'));
+ var num2 = number(prompt('Enter your number +'));
+ var namsum = (num1 +num2 );
+ console.log(numsum);
+
+ var num3 = number(prompt('Enter your number -'));
+ var num4 = number(prompt( 'Enter your number -'));
+ var numSum2=( num3 - num4 );
+ cansole.log(numSum2)
+
+ var num5 = number(prompt('Enter your number *'));
+ var num6 = number(prompt('Enter your number *'));
+ var numSum3 =( num5 * num6 );
+cansloe.log(numSum3);
+var num7 = number(prompt('Enter your number /'));
+var num8 = number(prompt('Enter your number /'));
+var numSum4 = (num7 /num8 );
+cansole.log(numSum);
